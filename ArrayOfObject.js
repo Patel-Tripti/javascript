e55b20b29d775eb64978ef4069bee1ae8bbd9ff0
Patel-Tripti <139382407+Patel-Tripti@users.noreply.github.com>
2024-12-31 10:40:17 +0530

    Add files via upload

---
+++ ArrayOfObject.js
@@ -0,0 +1,18 @@
+let student = [
+    {
+        name:'a',
+        age:21
+    },
+    {
+       name:'b',
+       age:41
+    },
+    {
+        name:'c',
+        age:22
+    }
+]
+for(let i=0;i<student.length;i++){
+  console.log(student[i].name);
+  
+}
